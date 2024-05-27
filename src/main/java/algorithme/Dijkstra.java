200d2c5c229eefb40eb40c15977063a505afa57a
Ali-ShanKM <167819439+Ali-ShanKM@users.noreply.github.com>
2024-05-27 07:05:25 +0200

    Update Dijkstra.java

--- src/main/java/algorithme/Dijkstra.java
+++ src/main/java/algorithme/Dijkstra.java
@@ -1,10 +1,30 @@
 package main.java.algorithme;
 
-import main.java.graphe.IGraphe;
+
+import main.java.graphe.IGrapheConst;
 
 import java.util.Map;
+import java.util.PriorityQueue;
 
 public class Dijkstra {
-    public static void dijkstra(IGraphe g, String source, Map<String, Integer> dist, Map<String, String> prev) {
+
+
+    public static void dijkstra(IGrapheConst g, String source, Map<String, Integer> dist, Map<String, String> prev) {
+        PriorityQueue<String> sommetsRestants = new PriorityQueue<>();
+        String sommetCourant = source;
+        sommetsRestants.add(sommetCourant);
+        dist.put(sommetCourant, 0);
+        while (!sommetsRestants.isEmpty()) {
+            sommetCourant = sommetsRestants.poll();
+            for (String succ : g.getSucc(sommetCourant)) {
+                int distance = g.getValuation(sommetCourant, succ) + dist.get(sommetCourant);
+                if (!dist.containsKey(succ) || distance < dist.get(succ)) {
+                    dist.put(succ, distance);
+                    prev.put(succ, sommetCourant);
+                    sommetsRestants.remove(succ);
+                    sommetsRestants.add(succ);
+                }
+            }
+        }
     }
 }
